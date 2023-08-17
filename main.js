adff05fbace3aca46933da5cebbe0784888de6a2
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-08-17 15:49:11 -0300

    commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//Declaração de variaveis
+
+let numero = prompt("Informe o numero desejado: ")
+
+
+//Verificar a operacao matematica selecionada
+if (numero % 2 === 0) {
+    document.write('Seu resultado é par ');
+  } else {
+    document.write('Seu resultado é impar');
+  }
+
+  
